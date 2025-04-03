8cf71230e0aa4f151a59cf539a88e0201cafe020
joaqukn <outomurourra.joaquinet36@gmail.com>
2025-04-03 04:54:39 -0300

    Creacion y agreado de las cosas

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "paginas",)));
+
+const img = [
+    { id: 1, imagenes1: 'img/descarga.png' },
+    { id: 2, imagenes1: 'img/fondo.gif' },
+];
+
+app.get('/img/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const imagen = img.find(i => i.id === id);
+    if (imagen) {
+        res.sendFile(path.join(__dirname, 'paginas', imagen.imagenes1));
+    } else {
+        res.status(404).send('Imagen no encontrada');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
+
