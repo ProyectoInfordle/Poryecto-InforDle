524cab6fe7a74ef820a17d305d982724ecb0faa9
ProyectoInfordle <tumianpro@gmail.com>
2025-04-10 11:41:12 -0300

    Cambios
    pinche barqui ponete a laburar vago

---
+++ InforDle/Poryecto-InforDle/paginas/script.js
@@ -0,0 +1,54 @@
+// Frases a mostrar
+const phrases = [
+    " Aprender",
+    " Entender",
+    " Descubrir",
+];
+
+// Función que simula el "auto typing"
+function typePhrase(spanElement, phrase, index = 0, callback) {
+    if (index < phrase.length) {
+        spanElement.textContent += phrase[index];
+        index++;
+        setTimeout(() => typePhrase(spanElement, phrase, index, callback), 100); // Velocidad de tipeo
+    } else {
+        // Cuando termine de escribir la frase, llamamos al callback para borrar la frase
+        setTimeout(callback, 1000); // Esperamos un segundo antes de empezar a borrar
+    }
+}
+
+// Función que simula el "auto deleting" (borrado)
+function deletePhrase(spanElement, phrase, index = phrase.length - 1, callback) {
+    if (index >= 0) {
+        spanElement.textContent = phrase.substring(0, index);
+        index--;
+        setTimeout(() => deletePhrase(spanElement, phrase, index, callback), 50); // Velocidad de borrado
+    } else {
+        setTimeout(callback, 500);
+    }
+}
+
+// Función para manejar las frases
+function startTypingAnimation() {
+    const typingSpan = document.querySelector('.typing-text span');
+    let currentPhraseIndex = 0;
+
+    function typeNextPhrase() {
+        const currentPhrase = phrases[currentPhraseIndex];
+        typingSpan.textContent = ''; // Limpiar el texto actual
+        typePhrase(typingSpan, currentPhrase, 0, () => {
+            // Después de escribir la frase, iniciamos el borrado
+            deletePhrase(typingSpan, currentPhrase, currentPhrase.length - 1, () => {
+                // Pasamos a la siguiente frase después de que se haya borrado la anterior
+                currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
+                typeNextPhrase(); // Llamamos de nuevo para escribir la siguiente frase
+            });
+        });
+    }
+
+    typeNextPhrase(); // Iniciar la animación
+}
+
+// Iniciar la animación cuando se cargue la página
+window.onload = startTypingAnimation;
+
